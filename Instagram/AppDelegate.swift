cc7c85d20bcde5c02c9eebf83216de37974c284a
FanXia0926 <78517859+FanXia0926@users.noreply.github.com>
2021-10-20 23:49:17 -0700

    instagram iOS

--- Instagram/AppDelegate.swift
+++ Instagram/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let parseConfig = ParseClientConfiguration{
+            $0.applicationId = "iOjcGGrXfwr8n9XGi0JC1MJG2ts3SdTq13fE3rFk"
+            $0.clientKey = "wDKKN71yZAqvBDHLGv83iMT5uH6g8HqmmEshx9S1"
+            $0.server = "https://parseapi.back4app.com/"
+        }
+        Parse.initialize(with: parseConfig)
         return true
     }
 
---
+++ Instagram/PostCell.swift
@@ -0,0 +1,12 @@
+//
+//  PostCell.swift
+//  Instagram
+//
+//  Created by Fan Xia on 10/20/21.
+//
+
+import UIKit
+
+class PostCell: UITabeViewCell {
+
+}
